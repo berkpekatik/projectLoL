a2a465590d71da554adf4dcd3ee17d64c8ba5624
Berk Pekatik <berkcanpekatik@gmail.com>
2018-01-22 15:36:34 +0300

    update

---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Windows.Forms;
+using System.IO;
+using System.Web.Script.Serialization;
+using System.Web.Script;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Resources;
+using System.Reflection;
+
+
+//api key = ?api_key=RGAPI-d58dcb7c-a18f-47cb-8696-f4908b1aa373
+//League Search https://tr1.api.riotgames.com/lol/league/v3/positions/by-summoner/642260?api_key=RGAPI-8fb6671b-56b9-4b9a-b31b-4f85e40b6e48
+//Summoner Search https://tr1.api.riotgames.com/lol/summoner/v3/summoners/by-name/Vols?api_key=RGAPI-8fb6671b-56b9-4b9a-b31b-4f85e40b6e48
+//https://tr1.api.riotgames.com/lol/league/v3/positions/by-summoner/2812929?api_key=RGAPI-8fb6671b-56b9-4b9a-b31b-4f85e40b6e48
+
+namespace ProjectLoL
+{
+    public partial class Form1 : Form
+    {
+        string apikey = "?api_key=RGAPI-d58dcb7c-a18f-47cb-8696-f4908b1aa373";
+        string aURL = "https://tr1.api.riotgames.com/lol/";
+        string searchTypeSummoner = "summoner/v3/summoners/by-name/";
+        string searchTypeLeague = "league/v3/positions/by-summoner/";
+        public Form2 f2;
+        JavaScriptSerializer javaScriptSerializer = new JavaScriptSerializer();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.FromArgb(0, 31, 63);
+            sumLabel.Text = f2.summonerName.ToString();
+            foreach (Label l in Controls.OfType<Label>())
+                l.BackColor = System.Drawing.Color.Transparent;
+            sumSearch("Vols");
+        }
+        void sumSearch(string id)
+        {
+            var json = new WebClient().DownloadString(aURL + searchTypeSummoner + id + apikey);
+            List<summonerSearch> listSumSearch = (List<summonerSearch>)javaScriptSerializer.Deserialize(json, typeof(List<summonerSearch>));
+            foreach (var sumsearch in listSumSearch)
+            {
+                sumIcon(sumsearch.profileIconId);
+                sumLeague(sumsearch.id);
+            }
+        }
+        void sumIcon(int profileIconId)
+        {
+            var request = WebRequest.Create("http://opgg-static.akamaized.net/images/profile_icons/profileIcon" + profileIconId + ".jpg");
+            using (var response = request.GetResponse())
+            using (var stream = response.GetResponseStream())
+            {
+                pictureBox1.Image = Bitmap.FromStream(stream);
+            }
+        }
+        void sumLeague(int id)
+        {
+            var jsun = new WebClient().DownloadString(aURL + searchTypeLeague + id + apikey);
+            List<summonerLeague> listSumLeague = (List<summonerLeague>)javaScriptSerializer.Deserialize(jsun, typeof(List<summonerLeague>));
+            foreach (summonerLeague sumleague in listSumLeague)
+            {
+                var request = WebRequest.Create("http://berkpekatik.com/rank/" + sumleague.tier + "_" + sumleague.rank + ".png");
+                using (var response = request.GetResponse())
+                using (var stream = response.GetResponseStream())
+                {
+                    pictureBox2.Image = Bitmap.FromStream(stream);
+                }
+                wText.Text = sumleague.wins.ToString();
+                lText.Text = sumleague.losses.ToString();
+            }
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
---
+++ Form2.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace ProjectLoL
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.sumName = new System.Windows.Forms.TextBox();
+            this.server = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.button1.Location = new System.Drawing.Point(166, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // sumName
+            // 
+            this.sumName.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.sumName.Location = new System.Drawing.Point(145, 26);
+            this.sumName.Name = "sumName";
+            this.sumName.Size = new System.Drawing.Size(121, 27);
+            this.sumName.TabIndex = 1;
+            // 
+            // server
+            // 
+            this.server.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.server.FormattingEnabled = true;
+            this.server.Items.AddRange(new object[] {
+            "TR",
+            "BR",
+            "EUNE",
+            "EUW",
+            "LAN",
+            "LAS",
+            "NA",
+            "OCE",
+            "RU",
+            "JP"});
+            this.server.Location = new System.Drawing.Point(145, 59);
+            this.server.Name = "server";
+            this.server.Size = new System.Drawing.Size(121, 27);
+            this.server.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(11, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Sumonner Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(85, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Server";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(312, 149);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.server);
+            this.Controls.Add(this.sumName);
+            this.Controls.Add(this.button1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox sumName;
+        private System.Windows.Forms.ComboBox server;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
